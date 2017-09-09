d5ca67765c5b194516850db3610f0da70e677c0e
Galiant <antonijodev@gmail.com>
2017-09-09 12:56:20 +0100

    Mobile header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+$(function() {
+    $(".header-text h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+});
+
 /*SMOOTH SCROLLING*/
 $(document).on('click', 'a[href*="#"]:not([href="#"])', function(e) {
 
@@ -22,3 +26,46 @@ $(document).on('click', 'a[href*="#"]:not([href="#"])', function(e) {
     }
 
 });
+
+/*!
+ * FitText.js 1.2
+ *
+ * Copyright 2011, Dave Rupert http://daverupert.com
+ * Released under the WTFPL license
+ * http://sam.zoy.org/wtfpl/
+ *
+ * Date: Thu May 05 14:23:00 2011 -0600
+ */
+
+(function($) {
+
+    $.fn.fitText = function(kompressor, options) {
+
+        // Setup options
+        var compressor = kompressor || 1,
+            settings = $.extend({
+                'minFontSize': Number.NEGATIVE_INFINITY,
+                'maxFontSize': Number.POSITIVE_INFINITY
+            }, options);
+
+        return this.each(function() {
+
+            // Store the object
+            var $this = $(this);
+
+            // Resizer() resizes items based on the object width divided by the compressor * 10
+            var resizer = function() {
+                $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+            };
+
+            // Call once to set.
+            resizer();
+
+            // Call on resize. Opera debounces their resize by default.
+            $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+        });
+
+    };
+
+})(jQuery);
